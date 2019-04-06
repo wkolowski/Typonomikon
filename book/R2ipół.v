0cb56cb5b17ae2fc59c9cfba938a57d898232e33
wkolowski <wkolowski@protonmail.com>
2019-04-06 23:48:49 +0200

    Prawie skończony podrozdział o rekursji strukturalnej.

--- book/R2ipół.v
+++ book/R2ipół.v
@@ -70,6 +70,12 @@
     bardzo upierdliwa, więc raczej nie będziemy jej używać. Skupmy się
     zatem na badaniu rekursji strukturalnej, dobrze ufundowanej i ogólnej. *)
 
+(** **** Ćwiczenie *)
+
+(** Przypomnij sobie podrozdział o indukcji wzajemnej. Następnie wytłumacz,
+    jak przetłumaczyć definicję funkcji za pomocą rekursji wzajemnej na
+    definicję, która nie używa rekursji wzajemnej. *)
+
 (** * Rekursja ogólna *)
 
 (** W Coqu rekursja ogólna nie jest dozwolona. Powód jest banalny: prowadzi
@@ -109,7 +115,7 @@ Fail Definition the_universe_explodes : False := loop tt.
    In environment
    loop : unit -> False
    u : unit
-   Recursive call to loop has principal argument equal to 
+   Recursive call to loop has principal argument equal to
    "u" instead of a subterm of "u".
    Recursive definition is: "fun u : unit => loop u". *)
 
@@ -121,6 +127,19 @@ Fail Definition the_universe_explodes : False := loop tt.
     rekurencyjnych, wróćmy na chwilę do indukcji. Jak się zaraz okaże,
     nielegalność rekursji ogólnej wymusza również pewne ograniczenia w
     definicjach induktywnych. *)
+
+(** **** Ćwiczenie *)
+
+(** Ograniczenia nakładane przez Coqa sprawiają, że wszystkie napisane
+    przez nas funkcje rekurencyjne muszą się kiedyś zatrzymać i zwrócić
+    ostateczny wynik swojego działania. Tak więc nie możemy w Coqu pisać
+    funkcji nieterminujących, czyli takich, które się nie zatrzymują.
+
+    Rozważ bardzo interesujące pytanie filozoficzne: czy funkcje, które
+    nigdy się nie zatrzymują (lub nie zatrzymują się tylko dla niektórych
+    argumentów) mogą być w ogóle do czegokolwiek przydatne?
+
+    Nie daj się wpuścić w maliny. *)
 
 (** * Ścisła pozytywność *)
 
@@ -142,11 +161,11 @@ Fail Inductive wut (A : Type) : Type :=
 (* Error: Non strictly positive occurrence of "wut"
    in "(wut A -> A) -> wut A". *)
 
-(** Żeby zrozumieć ten komunikat o błędzie, musimy najpierw przypomnieć sobie
-    składnię konstruktorów. Konstruktory typu induktywnego [T] będą mieć (w
-    dość sporym uproszczeniu) postać [arg1 -> ... -> argN -> T] — są to funkcje
-    biorące pewną (być może zerową) ilość argumentów, a ich przeciwdziedziną
-    jest definiowany typ [T].
+(** Żeby zrozumieć ten komunikat o błędzie, musimy najpierw przypomnieć
+    sobie składnię konstruktorów. Konstruktory typu induktywnego [T] będą
+    mieć (w dość sporym uproszczeniu) postać [arg1 -> ... -> argN -> T] —
+    są to funkcje biorące pewną (być może zerową) ilość argumentów, a ich
+    przeciwdziedziną jest definiowany typ [T].
 
     Jeżeli definiowany typ [T] nie występuje nigdzie w typach argumentów
     [arg1 ... argN], sytuacja jest klarowna i wszystko jest w porządku.
@@ -221,7 +240,8 @@ Fail Definition santa_is_a_pedophile : False := y False.
     - [| C8 : forall A : Type, (T -> A) -> T]
     - [| C9 : (forall x : T, T) -> T]
     - [| C10 : (forall (A : Type) (x : T), A) -> T]
-    - [| C11 : forall A B C : Type, A -> (forall x : T, B) -> (C -> T) -> T] *)
+    - [| C11 : forall A B C : Type,
+                  A -> (forall x : T, B) -> (C -> T) -> T] *)
 
 (* begin hide *)
 (* C1-C7 są legalne, C8-C11 nie. *)
@@ -229,10 +249,16 @@ Fail Definition santa_is_a_pedophile : False := y False.
 
 (** * Rekursja strukturalna *)
 
-(** Przyjrzyjmy się ponownie definicji dodawania: *)
+(** Wiemy już, że rekursja ogólna prowadzi do sprzeczności, a jedyną legalną
+    formą rekursji jest rekursja strukturalna. Funkcje rekurencyjne, które
+    dotychczas pisaliśmy, były strukturalnie rekurencyjne, więc potrafisz
+    już całkiem sprawnie posługiwać się tym rodzajem rekursji. Pozostaje
+    nam zatem zbadać jedynie techniczne detale dotyczące sposobu realizacji
+    rekursji strukturalnej w Coqu. W tym celu przyjrzyjmy się ponownie
+    definicji dodawania: *)
 
 Print plus.
-(* plus = 
+(* plus =
    fix plus (n m : nat) {struct n} : nat :=
      match n with
      | 0 => m
@@ -249,18 +275,34 @@ Print plus.
 
     Po drugie: za listą argumentów, a przed zwracanym typem, występuje
     adnotacja [{struct n}]. Wskazuje ona, który z argumentów funkcji
-    jest argumentem głównym. Definiując funkcje rekurencyjne zazwyczaj
-    nie musimy go pisać, gdyż Coq jest w stanie sam wywnioskować, który
-    argument jest główny.
+    jest argumentem głównym. Dotychczas gdy definiowaliśmy funkcje
+    rekurencyjne nigdy nie musieliśmy jej pisać, bo Coq zawsze domyślał
+    się, który argument ma być główny. W poetyckiej polszczyźnie argument
+    główny możemy wskazać mówiąc np., że "funkcja plus zdefiniowana jest
+    przez rekursję po pierwszym argumencie" albo "funkcja plus zdefinowana
+    jest przez rekursję po n".
 
-    Czym jest argument główny? Aby to zrozumieć, zbadajmy najpierw
-    relację bycia podtermem (dla uproszczenia, skupimy się na termach
-    typów induktywnych). Relację tę opisują dwie proste zasady:
-    - po pierwsze, jeżeli dany term został skonstruowany pewnym konstruktorem,
-      to jego podtermami są rekurencyjne argumenty konstruktora. Przykład:
-      [0] jest podtermem [S 0], zaś [nil] podtermem [cons 42 nil].
+    Czym jest argument główny? Spróbuję wyjasnić to w sposób operacyjny:
+    - jako argument główny możemy wskazać dowolny argument, którego typ
+      jest induktywny
+    - Coq wymusza na nas, aby argumentem głównym wywołania rekurencyjnego
+      był podterm argumentu głównego z obecnego wywołania *)
+
+(** Dlaczego taki zabieg chroni nas przed sprzecznością? Przypomnij sobie,
+    że termy typów induktywnych muszą być skończone. Parafrazując: są to
+    drzewa o skończonym rozmiarze. Ich podtermy są od nich mniejsze, więc
+    w kolejnych wywołaniach rekurencyjnych argument główny będzie malał,
+    aż w końcu jego rozmiar skurczy się do zera. Wtedy rekursja zatrzyma
+    się, bo nie będzie już żadnych podtermów, na których można by zrobić
+    wywołanie rekurencyjne.
+
+    Żeby lepiej zrozumieć ten mechanizm, zbadajmy najpierw relację bycia
+    podtermem dla typów induktywnych. Relację tę opisują dwie proste zasady:
+    - po pierwsze, jeżeli dany term został zrobiony jakimś konstruktorem,
+      to jego podtermami są rekurencyjne argumenty tego konstruktora.
+      Przykład: [0] jest podtermem [S 0], zaś [nil] podtermem [cons 42 nil].
     - po drugie, jeżeli [t1] jest podtermem [t2], a [t2] podtermem [t3],
-      to [t1] jest podtermem [t3] — własność ta nazywa się tranzytywnością.
+      to [t1] jest podtermem [t3] — własność ta nazywa się przechodniością.
       Przykład: [S 0] jest podtermem [S (S 0)], a zatem [0] jest podtermem
       [S (S 0)]. Podobnie [nil] jest podtermem [cons 666 (cons 42 nil)] *)
 
@@ -271,13 +313,13 @@ Print plus.
 (* begin hide *)
 Inductive subterm_nat : nat -> nat -> Prop :=
     | subterm_nat_S : forall n : nat, subterm_nat n (S n)
-    | subterm_nat_trans : forall x y z : nat,
+    | subterm_nat_trans' : forall x y z : nat,
         subterm_nat x y -> subterm_nat y z -> subterm_nat x z.
 
 Inductive subterm_list {A : Type} : list A -> list A -> Prop :=
     | subterm_list_cons : forall (h : A) (t : list A),
         subterm_list t (h :: t)
-    | subterm_list_trans : forall x y z : list A,
+    | subterm_list_trans' : forall x y z : list A,
         subterm_list x y -> subterm_list y z -> subterm_list x z.
 
 Inductive trans_clos {A : Type} (R : A -> A -> Prop) : A -> A -> Prop :=
@@ -324,10 +366,207 @@ Goal subterm_nat' 0 (S 0).
 Proof. do 2 constructor. Qed.
 (* end hide *)
 
-(** 
+(** **** Ćwiczenie *)
 
-    Argumentem głównym funkcji [plus] jest jej pierwszy argument (o czym
-    informuje nas adnotacja [{struct n}]), gdyż to on jest dopasowywany
-    jako pierwszy (i jedyny). W przypadku gdy [n] jest równe [S p], [plus]
-    jest wywoływany rekurencyjnie z argumentami [p] i [m], co jest dozwolone,
-    gdyż jego argument główny, [p], jest podtermem [S p]. *)
+(** Udowodnij, że relacje [subterm_nat] oraz [subterm_list] są antyzwrotne
+    i przechodnie. Uwaga: to może być całkiem trudne. *)
+
+(* begin hide *)
+Require Import Arith.
+
+Lemma subterm_nat_lt :
+  forall n m : nat, subterm_nat n m -> n < m.
+Proof.
+  induction 1.
+    apply le_n.
+    apply lt_trans with y; assumption.
+Qed.
+(* end hide *)
+
+Lemma subterm_nat_antirefl :
+  forall n : nat, ~ subterm_nat n n.
+(* begin hide *)
+Proof.
+  do 2 intro. apply Nat.lt_irrefl with n. apply subterm_nat_lt. assumption.
+Qed.
+(* end hide *)
+
+Lemma subterm_nat_trans :
+  forall a b c : nat,
+    subterm_nat a b -> subterm_nat b c -> subterm_nat a c.
+(* begin hide *)
+Proof.
+  intros. econstructor; eassumption.
+Qed.
+(* end hide *)
+
+(* begin hide *)
+Lemma subterm_nat'_lt :
+  forall n m : nat, subterm_nat' n m -> n < m.
+Proof.
+  induction 1.
+    inversion H; subst. apply le_n.
+    inversion H; subst. apply lt_trans with (S x).
+      apply Nat.lt_succ_diag_r.
+      assumption.
+Qed.
+
+Lemma subterm_nat'_antirefl :
+  forall n : nat, ~ subterm_nat' n n.
+Proof.
+  do 2 intro. apply Nat.lt_irrefl with n. apply subterm_nat'_lt. assumption.
+Qed.
+
+Lemma subterm_nat'_trans :
+  forall a b c : nat,
+    subterm_nat' a b -> subterm_nat' b c -> subterm_nat' a c.
+Proof.
+  intros a b c H. revert c. induction H; intros.
+    apply trans_clos_trans with y; assumption.
+    apply trans_clos_trans with y.
+      assumption.
+      apply IHtrans_clos. assumption.
+Qed.
+(* end hide *)
+
+(* begin hide *)
+Lemma subterm_list_length :
+  forall (A : Type) (l1 l2 : list A),
+    subterm_list l1 l2 -> length l1 < length l2.
+Proof.
+  induction 1; cbn.
+    apply le_n.
+    eapply lt_trans; eassumption.
+Qed.
+(* end hide *)
+
+Lemma subterm_list_antirefl :
+  forall (A : Type) (l : list A), ~ subterm_list l l.
+(* begin hide *)
+Proof.
+  repeat intro. eapply Nat.lt_irrefl, subterm_list_length. eassumption.
+Qed.
+(* end hide *)
+
+Lemma subterm_list_trans :
+  forall (A : Type) (l1 l2 l3 : list A),
+    subterm_list l1 l2 -> subterm_list l2 l3 ->
+      subterm_list l1 l3.
+(* begin hide *)
+Proof.
+  intros. eapply subterm_list_trans'; eassumption.
+Qed.
+(* end hide *)
+
+(* begin hide *)
+Lemma subterm_list'_length :
+  forall (A : Type) (l1 l2 : list A),
+    subterm_list' l1 l2 -> length l1 < length l2.
+Proof.
+  induction 1.
+    inversion H; subst. apply le_n.
+    inversion H; subst; cbn in *. apply lt_trans with (length (h :: x)).
+      cbn. apply le_n.
+      cbn. exact IHtrans_clos.
+Qed.
+
+Lemma subterm_list'_antirefl :
+  forall (A : Type) (l : list A), ~ subterm_list' l l.
+Proof.
+  repeat intro. eapply Nat.lt_irrefl, subterm_list'_length. eassumption.
+Qed.
+
+Lemma subterm_list'_trans :
+  forall (A : Type) (l1 l2 l3 : list A),
+    subterm_list' l1 l2 -> subterm_list' l2 l3 -> subterm_list' l1 l3.
+Proof.
+  intros A l1 l2 l3 H. revert l3. induction H; intros.
+    apply trans_clos_trans with y; assumption.
+    apply trans_clos_trans with y.
+      assumption.
+      apply IHtrans_clos. assumption.
+Qed.
+(* end hide *)
+
+(** Jak widać, podtermy liczby naturalnej to liczby naturalne, które są od
+    niej mniejsze, zaś podtermy listy to jej ogon, ogon ogona i tak dalej.
+    Zero i lista pusta nie mają podtermów, gdyż są to przypadki bazowe,
+    pochodzące od konstruktorów, które nie mają argumentów rekurencyjnych.
+
+    Dla każdego typu induktywnego możemy zdefiniować relację bycia podtermem
+    podobną do tych dla liczb naturalnych i list. Zauważmy jednak, że nie
+    możemy za jednym zamachem zdefiniować relacji bycia podtermem dla
+    wszystkich typów induktywnych, gdyż nie możemy w Coqu powiedzieć czegoś
+    w stylu "dla wszystkich typów induktywnych". Możemy powiedzieć jedynie
+    "dla wszystkich typów".
+
+    Coq nie generuje jednak automatycznie takiej relacji, gdy definiujemy
+    nowy typ induktywny. W jaki zatem sposób Coq sprawdza, czy jeden term
+    jest podtermem drugiego? Otóż... w sumie, to nie sprawdza. Rzućmy okiem
+    na następujący przykład: *)
+
+Fail Fixpoint weird (n : nat) : unit :=
+match n with
+    | 0 => tt
+    | S n' => weird 0
+end.
+
+(** Definicja zdaje się być poprawna: [0] to przypadek bazowy, a gdy [n]
+    jest postaci [S n'], wywołujemy funkcję rekurencyjnie na argumencie
+    [0]. [0] jest podtermem [S n'], a zatem wszystko powinno być dobrze.
+    Dostajemy jednak następujący komunikat o błędzie: *)
+
+(* Recursive definition of weird is ill-formed.
+   In environment
+   weird : nat -> unit
+   n : nat
+   n' : nat
+   Recursive call to weird has principal argument equal to 
+   "0" instead of "n'". *)
+
+(** Komunikat ten głosi, że argumentem głównym wywołania rekurencyjnego jest
+    [0], podczas gdy powinno być nim [n']. Wynika stąd jasno i wyraźnie, że
+    jedynymi legalnymi argumentami w wywołaniu rekurencyjnym są te podtermy
+    argumentamu głównego, które zostają ujawnione w wyniku dopasowania do
+    wzorca. Coq nie jest jednak głupi - jest głupszy, niż ci się wydaje, o
+    czym świadczy poniższy przykład. *)
+
+Fail Fixpoint fib (n : nat) : nat :=
+match n with
+    | 0 => 0
+    | 1 => 1
+    | S (S n') => fib n' + fib (S n')
+end.
+
+(** Tym razem komunikat o błędzie wygląda następująco: *)
+
+(* Recursive definition of fib is ill-formed.
+   In environment
+   fib : nat -> nat
+   n : nat
+   n0 : nat
+   n' : nat
+   Recursive call to fib has principal argument equal to 
+   "S n'" instead of one of the following variables: 
+   "n0" "n'". *)
+
+(** Mimo, że [S n'] jest podtermem [S (S n')], który pochodzi z dopasowania
+    do wzorca, to Coq nie jest w stanie zauważyć tego faktu. W komunikacie
+    o błędzie pojawia się za to tajemnicza zmienna [n0], której w naszym
+    kodzie nigdzie nie ma. Sposobem na poradzenie sobie z problemem jest
+    pokazanie Coqowi palcem, o co nam chodzi: *)
+
+Fixpoint fib (n : nat) : nat :=
+match n with
+    | 0 => 0
+    | 1 => 1
+    | S (S n' as n'') => fib n' + fib n''
+end.
+
+(** Tym razem Coq widzi, że [S n'] jest podtermem [S (S n')], gdyż explicite
+    nadaliśmy temu termowi nazwę [n''], używają do tego klauzli [as]. Cóż...
+    kto powiedział, że z maszyną łatwo będzie się dogadać? *)
+ 
+(** * Rekursja dobrze ufundowana *)
+
+(** * Indukcja funkcyjna *)
