8c178dc5b301c15d1983877d6c3e979f86cb52af
wkolowski <wkolowski@protonmail.com>
2019-10-03 23:10:37 +0200

    Udało się zrobić generyczne zipWith.

--- code/zipWithN.v
+++ code/zipWithN.v
@@ -2,42 +2,85 @@ Require Import List.
 Import ListNotations.
 
 Inductive Code : Type :=
-  | Last : Type -> Type -> Code
+  | Singl : Type -> Code
   | Cons : Type -> Code -> Code.
 
-Fixpoint funType (c : Code) : Type :=
+Fixpoint cmap (F : Type -> Type) (c : Code) : Code :=
 match c with
-    | Last A B => A -> B
-    | Cons A c' => A -> funType c'
+  | Singl A => Singl (F A)
+  | Cons A c' => Cons (F A) (cmap F c')
 end.
 
-Fixpoint listType (c : Code) : Type :=
+Fixpoint funType (c : Code) (R : Type) : Type :=
 match c with
-    | Last A B => list A -> list B
-    | Cons A c' => list A -> listType c'
+  | Singl A => A -> R
+  | Cons A c' => A -> funType c' R
 end.
 
-Fixpoint NIL (c : Code) : listType c :=
+Definition listType (c : Code) (R : Type) : Type :=
+  funType (cmap list c) R.
+
+Fixpoint prod (c : Code) : Type :=
 match c with
-    | Last A B => fun _ : list A => []
-    | Cons A c' => fun _ : list A => NIL c'
+  | Singl A => A
+  | Cons A c' => A * prod c'
 end.
 
-(*
-Fixpoint CONS (c : Code) : listType c :=
-match c with
-    | 
-*)
+Definition prodList (c : Code) : Type :=
+  prod (cmap list c).
 
-Fixpoint zipWith (c : Code) (f : funType c) {struct c} : listType c.
-Proof.
-  destruct c as [A B | A c']; cbn in *.
-    exact (map f).
-    induction 1 as [| h t].
-      exact (NIL c').
-      specialize (f h). specialize (zipWith _ f). exact zipWith.
-Defined.
+Definition listProd (c : Code) : Type :=
+  list (prod c).
+
+Definition uncurriedFunType (c : Code) (R : Type) : Type :=
+  prod c -> R.
+
+Definition uncurriedListType (c : Code) (R : Type) : Type :=
+  prodList c -> R.
+
+Fixpoint zip2 {A B : Type} (l : list A) (r : list B) : list (A * B) :=
+match l, r with
+  | [], _ => []
+  | _, [] => []
+  | hl :: tl, hr :: tr => (hl, hr) :: zip2 tl tr
+end.
+
+Fixpoint zip {c : Code} : prodList c -> listProd c :=
+match c with
+  | Singl A => id
+  | Cons A c' =>
+    fun '(l, p) => zip2 l (zip p)
+end.
 
 Compute
-  zipWith (Cons nat (Cons nat (Last nat nat))) (fun a b c => a + b + c)
-  [1; 2; 3] [4; 5; 6] [7; 8; 9].
+  @zip
+    (Cons bool (Singl nat))
+    ([true; false], [4; 5]).
+
+
+Fixpoint uncurryFun
+  {c : Code} {R : Type} {struct c} : funType c R -> uncurriedFunType c R.
+Proof.
+  destruct c; cbn in *; intro f; red; cbn.
+    exact f.
+    intros [t p]. exact (uncurryFun _ _ (f t) p).
+Defined.
+
+Fixpoint curryList
+  {c : Code} {R : Type} {struct c} : uncurriedListType c R -> listType c R.
+Proof.
+  destruct c as [A | A c']; unfold uncurriedListType; cbn in *.
+    intros f l. exact (f l).
+    intros f l. apply curryList. red. intro. apply f. split; assumption.
+Defined.
+
+
+Definition zipWith
+  {c : Code} {R : Type} (f : funType c R) : listType c (list R) :=
+    curryList (fun p : prodList c => map (uncurryFun f) (zip p)).
+
+Compute
+  @zipWith
+    (Cons nat (Cons nat (Singl nat))) _
+    (fun a b c => a + b + c)
+    [1; 2; 3] [4; 5; 6] [7; 8; 9].
