7efe9901bdc63343195f716cf509561eb5c338b3
wkolowski <wkolowski@protonmail.com>
2019-11-15 23:07:25 +0100

    Jeszcze więcej walki z permutacjami.

--- code/perms2.v
+++ code/perms2.v
@@ -21,6 +21,78 @@ Proof.
     rewrite length_map, IHt. reflexivity.
 Qed.
 
+Fixpoint nsum (l : list nat) : nat :=
+match l with
+    | [] => 0
+    | h :: t => h + nsum t
+end.
+
+Lemma len_join :
+  forall (A : Type) (ll : list (list A)),
+    length (join ll) = nsum (map length ll).
+Proof.
+  induction ll as [| h t]; cbn.
+    reflexivity.
+    rewrite length_app, IHt. reflexivity.
+Qed.
+
+Lemma len_perms :
+  forall (A : Type) (l : list A),
+    length (perms l) = fact (length l).
+Proof.
+  induction l as [| h t]; cbn.
+    reflexivity.
+    rewrite bind_spec, len_join, map_map.
+Abort.
+
+Lemma map_ins :
+  forall (A B : Type) (f : A -> B) (x : A) (l : list A),
+    map (map f) (ins x l) = ins (f x) (map f l).
+Proof.
+  induction l as [| h t]; cbn.
+    reflexivity.
+    f_equal. rewrite <- IHt, !map_map. cbn. reflexivity.
+Qed.
+
+Lemma ins_app :
+  forall (A : Type) (x : A) (l1 l2 : list A),
+    l1 = [] \/
+    l2 = [] \/
+      ins x (l1 ++ l2) =
+      map (fun l => app l l2) (ins x l1) ++
+      map (app l1) (ins x l2).
+Proof.
+  induction l1 as [| h1 t1]; cbn.
+    left. reflexivity.
+    induction l2 as [| h2 t2]; cbn.
+      right. left. reflexivity.
+      do 2 right. decompose [or] IHt2; subst; clear IHt2.
+        inversion H.
+Abort.
+
+Lemma ins_snoc :
+  forall (A : Type) (x y : A) (l : list A),
+    ins x (snoc y l) =
+    snoc (snoc x (snoc y l)) (map (snoc y) (ins x l)).
+Proof.
+  induction l as [| h t]; cbn.
+    reflexivity.
+    f_equal. rewrite IHt. rewrite map_snoc, !map_map.
+      cbn. reflexivity.
+Qed.
+
+Lemma ins_rev :
+  forall (A : Type) (x : A) (l : list A),
+    ins x (rev l) = rev (map rev (ins x l)).
+Proof.
+  induction l as [| h t]; cbn.
+    reflexivity.
+    rewrite <- !snoc_app_singl. rewrite ins_snoc, IHt.
+      rewrite map_rev, map_map, <- map_rev. f_equal.
+      rewrite map_rev, map_map. cbn. f_equal.
+      f_equal.
+Admitted.
+
 Lemma elem_ins :
   forall (A : Type) (x : A) (l : list A),
     elem (x :: l) (ins x l).
@@ -87,12 +159,6 @@ Proof.
     reflexivity.
     rewrite !bind_spec, !map_app in *.
 Abort.
-
-Fixpoint nsum (l : list nat) : nat :=
-match l with
-    | [] => 0
-    | h :: t => h + nsum t
-end.
 
 Lemma count_join :
   forall (A : Type) (p : A -> bool) (l : list (list A)),
