4373be759b2c5311b251fccbafc724c67bcd34d7
wkolowski <wkolowski@protonmail.com>
2020-02-12 23:53:15 +0100

    Opowieści o algebrach początkowych idą jak krew z nosa, ale przynajmniej mam już wszystko obmyślone.

--- code/Indukcja/naj.v
+++ code/Indukcja/naj.v
@@ -20,11 +20,11 @@
     ma ścisłej definicji - używam tego słowa, żeby ułatwić pracę twojej
     wyobraźni.
 
-    Czym jest kształt termu? Najprościej rzecz ujmując, jest to drzewko,
-    którego korzeniem jest konstrukt językowy (konstruktor, uprzednio
-    zdefiniowana funkcja lub stała, dopasowanie do wzorca, [let], lub
-    cokolwiek innego) użyty do stworzenia termu, a poddrzewa to argumenty
-    tego konstruktu.
+    Czym jest kształt termu? Najprościej rzecz ujmując każdy term jest
+    drzewkiem, którego korzeniem jest jakiś konstrukt językowy (stała,
+    konstruktor, uprzednio zdefiniowana funkcja, dopasowanie do wzorca,
+    [let], lub cokolwiek innego), a jego poddrzewa to argumenty tego
+    konstruktu.
 
     Dla przykładu, termy typu [nat] mogą mieć takie kształty:
     - [0] - stała
@@ -35,6 +35,68 @@
     - [length [true; false]] - uprzednio zdefiniowana funkcja
     - [let x := Prop in 16] - [let]
     - ... i wiele, wiele innych!
+
+    Tak wiele różnych sposobów robienia termów to niesamowite bogactwo,
+    więc żeby zgodnie z przysłowiem od tego przybytku nie rozbolała nas
+    głowa, musimy pomyśleć o nich w nieco bardziej jednorodny sposób.
+    Rozwiązanie jest na szczęście bajecznie proste: zauważ, że wszystkie
+    powyższe konstrukty językowe można po prostu zawinąć w funkcję, która
+    bierze pewną liczbę argumentów (być może zero) i zwraca coś typu
+    [nat].
+
+    To jednak nie w pełni mityguje przyszły-niedoszły ból głowy. O ile
+    mamy teraz jednorodny sposób myślenia o kształtach termów, to i tak
+    kształtów tych mogą być olbrzymie ilości. Z tego powodu dokonamy
+    samoograniczenia i zamiast o wszystkich możliwych kształtach termów
+    będziemy wybiórczo skupiać naszą uwagę tylko na tych kształtach,
+    które akurat będą nas interesować.
+
+    Dla przykładu, możemy interesować się termami typu [nat] zrobionymi
+    wyłącznie za pomocą:
+    - konstruktorów [0] i [S]
+    - konstruktora [0], stałej [1] oraz funkcji [plus 2]
+    - funkcji [plus] i stałych [5] oraz [42]
+    - funkcji [mult] i stałej [1]
+    - fukncji [length : list nat -> nat] *)
+
+(** **** Ćwiczenie *)
+
+(** Narysuj jakieś nietrywialne termy typu [nat] o takich kształtach. *)
+
+(* begin hide *)
+Module wut.
+Require Import List.
+Import ListNotations.
+
+Definition hehe : nat :=
+  length [@length nat []; length [@length nat []; @length nat []]].
+End wut.
+(* end hide *)
+
+(** **** Ćwiczenie *)
+
+(** Liczbę [n] da się wyrazić za pomocą termu [t], jeżeli [t] oblicza
+    się do [n], tzn. komenda [Compute t] daje w wyniku [n].
+
+    Pytanie: termy o których z powyższych kształtów mogą wyrazić
+    wszystkie liczby naturalne? *)
+
+(** **** Ćwiczenie *)
+
+(** Liczba [n] ma unikalną reprezentację za pomocą termów o danym
+    kształcie, gdy jest tylko jeden term [t], który reprezentuje [n].
+
+    Pytanie: które z powyższych sposobów unikalnie reprezentują
+    wszystkie liczby naturalne? *)
+
+
+
+
+
+(
+ choć wieść ludowa niesie, że od przybytku głowa nie boli (tak mówił
+    Owal i Ascetoholix), to tym
+    razem akurat przysłowie nie ma racji.
 
     Istnienie tak wielu kształtów jest dość problematyczne, więc
     musimy nieco uprościć sobie życie. Po pierwsze zauważmy, że
