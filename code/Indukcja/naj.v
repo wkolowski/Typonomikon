644d522263ab88a363c0bd84c03c9f8234e46d9f
wkolowski <wkolowski@protonmail.com>
2020-02-15 15:51:56 +0100

    Spora część opisu algebr początkowych, brakuje tylko konkluzji i jakichś mądrych zadań.

--- code/Indukcja/naj.v
+++ code/Indukcja/naj.v
@@ -13,7 +13,8 @@
 
     Nie jest to jednak jedyny sposób patrzenia na typy induktywne. W tym
     podrozdziale spróbuję przedstawić inny, w którym typ induktywny to
-    najlepszy typ do robienia termów o pewnym kształcie.
+    najlepszy typ do robienia termów o pewnym kształcie, a rekursja to
+    zmiana kształtu z lepszego na gorszy, ale bardziej użyteczny.
 
     Żeby móc patrzeć z tej perspektywy musimy najpierw ustalić, czym
     jest kształt. Uwaga: "kształt" nie jest pojęciem technicznym i nie
@@ -57,7 +58,7 @@
     - konstruktora [0], stałej [1] oraz funkcji [plus 2]
     - funkcji [plus] i stałych [5] oraz [42]
     - funkcji [mult] i stałej [1]
-    - fukncji [length : list nat -> nat] *)
+    - funkcji [length : list nat -> nat] *)
 
 (** **** Ćwiczenie *)
 
@@ -89,21 +90,60 @@ End wut.
     Pytanie: które z powyższych sposobów unikalnie reprezentują
     wszystkie liczby naturalne? *)
 
+(** Sporo już osiągnęliśmy w wyklarowywaniu pojęcia kształtu, ale
+    zatrzymajmy się na chwilę i zastanówmy się, czy jest ono zgodne
+    z naszymi intuicjami.
 
+    Okazuje się, że otóż nie do końca, bo w naszej obecnej formulacji
+    kształty [(0, plus)] oraz [(1, mult)] są różne, podczas gdy obrazki
+    jasno pokazują nam, że np. [plus 0 (plus 0 0)] oraz [mult 1 (mult 1 1)]
+    wyglądają bardzo podobnie, tylko nazwy są różne.
 
+    Dlatego też modyfikujemy nasze pojęcie kształtu - teraz kształtem
+    zamiast stałych i funkcji, jak [0] i [plus], nazywać będziemy typy
+    tych stałych i funkcji. Tak więc kształtem termów zrobionych z [0]
+    i [plus] będzie [nat] (bo [0 : nat]) i [nat -> nat -> nat] (bo
+    [plus : nat -> nat -> nat]). Teraz jest już jasne, że [1] i [mult]
+    dają dokładnie ten sam kształt, bo typem [1] jest [nat], zaś typem
+    [mult] jest [nat -> nat -> nat].
 
+    Zauważmy, że można nasze pojęcie kształtu jeszcze troszkę uprościć:
+    - po pierwsze, każdą stałą można traktować jako funkcję biorącą
+      argument typu [unit], np. możemy [0 : nat] reprezentować za pomocą
+      funkcji [Z : unit -> nat] zdefiniowanej jako
+      [Z := fun _ : unit => 0]
+    - po drugie, funkcje biorące wiele argumentów możemy reprezentować za
+      pomocą funkcji biorących jeden argument, np.
+      [plus : nat -> nat -> nat] możemy reprezentować za pomocą
+      [plus' : nat * nat -> nat], który jest zdefiniowany jako
+      [plus' := fun '(n, m) => plus n m]
+    - po trzecie, ponieważ kodziedzina wszystkich funkcji jest taka
+      sama (w naszym przypadku [nat]), możemy połączyć wiele funkcji w
+      jedną, np. [0] i [plus] możemy razem reprezentować jako
+      [Zplus : unit + nat * nat -> nat], zdefiniowaną jako
+      [Zplus := fun x => match x with | inl _ => 0 | inr (n, m) => plus n m end]
 
-(
- choć wieść ludowa niesie, że od przybytku głowa nie boli (tak mówił
-    Owal i Ascetoholix), to tym
-    razem akurat przysłowie nie ma racji.
+    Dzięki tym uproszczeniom (albo utrudnieniom, zależy kogo spytacie)
+    możemy teraz jako kształt traktować nie funkcje albo same ich typy,
+    lecz tylko jeden typ, który jest dziedziną takiej połączonej funkcji.
+    Tak więc zarówno [(0, plus)] jak i [(1, mult)] są kształtu
+    [unit + nat * nat]. Ma to sporo sensu: drzewa reprezentujące te termy
+    są albo liściem (reprezentowanym przez [unit]), albo węzłem, który
+    rozgałęzia się na dwa poddrzewa (reprezentowanym jako [nat * nat]).
 
-    Istnienie tak wielu kształtów jest dość problematyczne, więc
-    musimy nieco uprościć sobie życie. Po pierwsze zauważmy, że
-    każdy konstrukt językowy możemy zawinąć w funkcję, a zatem
-    wszystkie powyższe "kształty" sprowadzają się tak naprawdę do
-    funkcji o przeciwdziedzinie [nat], zaaplikowanych do jakichś
-    argumentów (np. [0] to funkcja [f : unit -> nat :=] zdefiniowana
-    jako [f _ := 0], zaaplikowana do [tt : unit].
+    Ale to jeszcze nie wszystko. Przecież [nat] to nie jedyny typ, w
+    którym można robić termy o kształcie [unit + nat * nat]. Jeżeli
+    przyjrzymy się, jak wyglądają termy zrobione za pomocą ([true, andb])
+    albo [(false, orb)], to okaże się, że... mają one dokładnie ten sam
+    kształt, mimo że według naszej definicji ich kształt to
+    [unit + bool * bool], czyli niby coś innego.
 
-*)
+    Ostatnim stadium ewolucji naszego pojęcia kształtu jest taki oto
+    zestaw definicji:
+    - kształt to funkcja [F : Type -> Type]
+    - realizacją kształtu [F] jest typ [X] oraz funkcja [f : F X -> X]
+
+    Widzimy teraz, że [(0, plus)], [(1, mult)], [(true, andb)] oraz
+    [(false, orb)] nie są kształtami, lecz realizacjami kształtu
+    [F := fun X : Type => 1 + X * X].
+    
