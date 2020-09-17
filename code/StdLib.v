1127a82109f7ca589eed4d79cc4f62b5e78463d1
wkolowski <wkolowski@protonmail.com>
2020-09-17 23:25:35 +0200

    Więcej rozważań o bibliotece standardowej.

--- code/StdLib.v
+++ code/StdLib.v
@@ -1,8 +1,3 @@
-(** TODO: inne pomysły na bibliotekę standardową Coqa: klasy na liczby
-    (coby nat, BinNat, N etc. miały taki sam interfejs; to samo dla
-    liczb wymiernych, ale nie np. dla floatów, które są czymś innym)
-    i listy, HoTTowa baza *)
-
 Require Import Recdef Bool.
 
 Inductive Even : nat -> Prop :=
@@ -180,6 +175,28 @@ End StrongInequality.
 
 (** TODO: klasy są słuszniejsze niż moduły *)
 
+Class Pointed (A : Type) : Type :=
+{
+    point : A;
+}.
+
+Class Inhabited (A : Type) : Prop :=
+{
+    inhabitant : A;
+}.
+
+Instance Inhabited_Pointed
+  (A : Type) (inst : Pointed A)
+  : Inhabited A :=
+{
+    inhabitant := point;
+}.
+
+Instance Pointed_nat : Pointed nat :=
+{
+    point := 0
+}.
+
 Module StrongNegation.
 
 Class StrongNegation (A : Type) : Type :=
@@ -195,6 +212,28 @@ Instance StrongNegation_Default
   (A : Type) : StrongNegation A | 999 :=
 {
     NOT := A -> False;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_Pointed
+  (A : Type) (inst : Pointed A)
+  : StrongNegation A :=
+{
+    NOT := False;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_Inhabited
+  (A : Type) (inst : Inhabited A)
+  : StrongNegation A :=
+{
+    NOT := False;
 }.
 Proof.
   trivial.
@@ -316,14 +355,35 @@ Proof.
     assumption.
 Defined.
 
-Class Inhabited (A : Type) : Prop :=
+Compute NOT (forall n : nat, True).
+Compute NOT (NOT (forall n : nat, True)).
+Compute NOT nat.
+
+End StrongNegation.
+
+Module StrongNegation_Prop.
+
+Class StrongNegation (A : Prop) : Type :=
 {
-    inhabitant : A;
+    NOT : Prop;
+    NOT_spec : NOT -> A -> False;
 }.
 
+Arguments NOT A {StrongNegation}.
+
 #[refine]
-Instance StrongNegation_Inhabited
-  (A : Type) (inst : Inhabited A)
+Instance StrongNegation_Default
+  (A : Prop) : StrongNegation A | 999 :=
+{
+    NOT := A -> False;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_Pointed
+  (A : Prop) (inst : Pointed A)
   : StrongNegation A :=
 {
     NOT := False;
@@ -332,11 +392,385 @@ Proof.
   trivial.
 Defined.
 
-Instance Inhabited_nat : Inhabited nat :=
+#[refine]
+Instance StrongNegation_Inhabited
+  (A : Prop) (inst : Inhabited A)
+  : StrongNegation A :=
 {
-    inhabitant := 0
+    NOT := False;
 }.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_True :
+  StrongNegation True :=
+{
+    NOT := False;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_False :
+  StrongNegation False :=
+{
+    NOT := True;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_or
+  (P Q : Prop) (PSN : StrongNegation P) (QSN : StrongNegation Q)
+  : StrongNegation (P \/ Q) :=
+{
+    NOT := NOT P /\ NOT Q;
+}.
+Proof.
+  destruct 1, 1.
+    apply PSN; assumption.
+    apply QSN; assumption.
+Defined.
+
+#[refine]
+Instance StrongNegation_and
+  (P Q : Prop) (PSN : StrongNegation P) (QSN : StrongNegation Q)
+  : StrongNegation (P /\ Q) :=
+{
+    NOT := NOT P \/ NOT Q;
+}.
+Proof.
+  destruct 1, 1.
+    apply PSN; assumption.
+    apply QSN; assumption.
+Defined.
+
+#[refine]
+Instance StrongNegation_impl
+  (P Q : Prop) (QSN : StrongNegation Q)
+  : StrongNegation (P -> Q) :=
+{
+    NOT := P /\ NOT Q;
+}.
+Proof.
+  destruct 1. intro pq. apply QSN.
+    assumption.
+    apply pq. assumption.
+Defined.
+
+#[refine]
+Instance StrongNegation_forall
+  (A : Type) (P : A -> Prop)
+  (PSN : forall x : A, StrongNegation (P x))
+  : StrongNegation (forall x : A, P x) :=
+{
+    NOT := exists x : A, NOT (P x);
+}.
+Proof.
+  intros [x np] H.
+  apply (PSN x).
+    assumption.
+    apply H.
+Defined.
+
+#[refine]
+Instance StrongNegation_exists
+  (A : Type) (P : A -> Prop)
+  (PSN : forall x : A, StrongNegation (P x))
+  : StrongNegation (exists x : A, P x) :=
+{
+    NOT := forall x : A, NOT (P x);
+}.
+Proof.
+  intros H [x np]. apply (PSN x).
+    apply H.
+    assumption.
+Defined.
 
 Compute NOT (forall n : nat, True).
 Compute NOT (NOT (forall n : nat, True)).
-Compute NOT nat.
+
+End StrongNegation_Prop.
+
+Module StrongNegation_Type.
+
+Class StrongNegation (A : Type) : Type :=
+{
+    TNOT : Type;
+    TNOT_spec : TNOT -> A -> False;
+}.
+
+Arguments TNOT A {StrongNegation}.
+
+#[refine]
+Instance StrongNegation_Default
+  (A : Type) : StrongNegation A | 999 :=
+{
+    TNOT := A -> False;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_Pointed
+  (A : Type) (inst : Pointed A)
+  : StrongNegation A :=
+{
+    TNOT := False;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_Inhabited
+  (A : Type) (inst : Inhabited A)
+  : StrongNegation A :=
+{
+    TNOT := False;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_True :
+  StrongNegation unit :=
+{
+    TNOT := False;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_False :
+  StrongNegation False :=
+{
+    TNOT := True;
+}.
+Proof.
+  trivial.
+Defined.
+
+#[refine]
+Instance StrongNegation_or
+  (A B : Type) (PSN : StrongNegation A) (QSN : StrongNegation B)
+  : StrongNegation (A + B) :=
+{
+    TNOT := TNOT A * TNOT B;
+}.
+Proof.
+  destruct 1, 1.
+    apply PSN; assumption.
+    apply QSN; assumption.
+Defined.
+
+#[refine]
+Instance StrongNegation_and
+  (A B : Type) (PSN : StrongNegation A) (QSN : StrongNegation B)
+  : StrongNegation (A * B) :=
+{
+    TNOT := TNOT A + TNOT B;
+}.
+Proof.
+  destruct 1, 1.
+    apply PSN; assumption.
+    apply QSN; assumption.
+Defined.
+
+#[refine]
+Instance StrongNegation_fun
+  (A B : Type) (QSN : StrongNegation B)
+  : StrongNegation (A -> B) :=
+{
+    TNOT := A * TNOT B;
+}.
+Proof.
+  destruct 1. intro pq. apply QSN.
+    assumption.
+    apply pq. assumption.
+Defined.
+
+#[refine]
+Instance StrongNegation_pi
+  (A : Type) (P : A -> Type)
+  (PSN : forall x : A, StrongNegation (P x))
+  : StrongNegation (forall x : A, P x) :=
+{
+    TNOT := {x : A & TNOT (P x)};
+}.
+Proof.
+  intros [x np] H.
+  apply (PSN x).
+    assumption.
+    apply H.
+Defined.
+
+#[refine]
+Instance StrongNegation_subset
+  (A : Type) (P : A -> Prop)
+  (PSN : forall x : A, StrongNegation (P x))
+  : StrongNegation {x : A | P x} :=
+{
+    TNOT := forall x : A, TNOT (P x);
+}.
+Proof.
+  intros H [x np]. apply (PSN x).
+    apply H.
+    assumption.
+Defined.
+
+#[refine]
+Instance StrongNegation_sigma
+  (A : Type) (P : A -> Type)
+  (PSN : forall x : A, StrongNegation (P x))
+  : StrongNegation {x : A & P x} :=
+{
+    TNOT := forall x : A, TNOT (P x);
+}.
+Proof.
+  intros H [x np]. apply (PSN x).
+    apply H.
+    assumption.
+Defined.
+
+Compute TNOT (forall n : nat, True).
+Compute TNOT (TNOT (forall n : nat, True)).
+Compute TNOT nat.
+
+End StrongNegation_Type.
+
+(** Klasy zbierające poszczególne implementacje izomorficznych typów są
+    w ogólności potrzebne nawet jeżeli mamy homotopiczną teorię typów,
+    bo przecież używanie izomorfizmów zrobionych ze ścieżek kosztuje. *)
+
+Module Natural.
+
+Class Natural (A : Type) : Type :=
+{
+    zero : A;
+    succ : A -> A;
+    rect :
+      forall P : A -> Type,
+        P zero -> (forall n : A, P n -> P (succ n)) ->
+          forall n : A, P n;
+
+    add : A -> A -> A;
+    sub : A -> A -> A;
+
+    mul : A -> A -> A;
+
+    pred : A -> option A;
+}.
+
+Instance Natural_nat : Natural nat :=
+{
+    zero := 0;
+    succ := S;
+    rect := nat_rect;
+
+    add := plus;
+    sub := minus;
+
+    mul := mult;
+
+    pred n := match n with | 0 => None | S n' => Some n' end
+}.
+
+End Natural.
+
+Module Integral.
+
+Class Integral (A : Type) : Type :=
+{
+    zero : A;
+    succ : A -> A;
+    pred : A -> A;
+
+    add : A -> A -> A;
+    sub : A -> A -> A;
+
+    mul : A -> A -> A;
+}.
+
+Require Import ZArith.
+
+Instance Integral_Z : Integral Z :=
+{
+    zero := 0;
+    succ := Z.succ;
+    pred := Z.pred;
+
+    add := Z.add;
+    sub := Z.sub;
+
+    mul := Z.mul;
+}.
+
+End Integral.
+
+Module Rational.
+
+Require Import QArith.
+
+Class Rational (A : Type) : Type :=
+{
+    zero : A;
+
+    add : A -> A -> A;
+    sub : A -> A -> A;
+
+    mul : A -> A -> A;
+    div : A -> A -> option A;
+}.
+
+#[refine]
+Instance Rational_Q : Rational Q :=
+{
+    zero := 0;
+    add := Qplus;
+    sub := Qminus;
+
+    mul := Qmult;
+(*     div := Qdiv; *)
+}.
+Admitted.
+
+End Rational.
+
+Module Sequence.
+
+Class Sequence (A L : Type) : Type :=
+{
+    nil : L;
+    cons : A -> L -> L;
+
+    rect :
+      forall P : L -> Type,
+        P nil -> (forall (h : A) (t : L), P t -> P (cons h t)) ->
+          forall l : L, P l;
+
+    snoc : L -> A -> L;
+
+    app : L -> L -> L;
+    rev : L -> L -> L;
+
+    (* no map with this definition *)
+}.
+
+End Sequence.
+
+(** TODO:
+    - HoTTowa baza
+    - porządne rozwiązanie rozstrzygalności
+    - hierarchia algebraiczna
+    - typy skończone
+
+
+*)
