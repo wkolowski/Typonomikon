b9145c2a4a8d0fdf2ec53c7a052e42dbdd04cf04
wkolowski <wkolowski@protonmail.com>
2022-01-26 23:07:13 +0100

    Zabawa komendami do wyświetlania tabelek i opcji.

---
+++ code/PrintingTable.v
@@ -0,0 +1,29 @@
+Module xd.
+
+Record R : Type := MkR
+{
+    field : unit;
+}.
+
+(* Add Printing Record Prestate.
+Add Printing Record Observation.
+Add Printing Record Premessage. *)
+
+Print Tables.
+Print Table Printing Record.
+Print Table Printing Coercion.
+Print Table Printing If.
+Print Table Printing Let.
+Print Table Printing Record.
+Print Table Printing Constructor.
+
+Local Unset Printing Records.
+
+Goal forall u1 u2 : unit, MkR u1 = MkR u2.
+Abort.
+
+End xd.
+
+Import xd.
+
+Goal forall u1 u2 : unit, MkR u1 = MkR u2.
