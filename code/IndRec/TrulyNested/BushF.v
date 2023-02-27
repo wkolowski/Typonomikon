d425aa7080a4818d51fa625b165591887d9fcb1f
wkolowski <wkolowski@protonmail.com>
2023-02-27 23:26:19 +0100

    Jakieś schemato-rekursjowe fikołki z typem krzaków.

---
+++ code/IndRec/TrulyNested/BushF.v
@@ -0,0 +1,15 @@
+Inductive BushF (F : Type -> Type) (A : Type) : Type :=
+| E : BushF F A
+| N : A -> F (F A) -> BushF F A.
+
+Arguments E {F A}.
+Arguments N {F A} _ _.
+
+Definition mapF
+  {F : Type -> Type} {A B : Type}
+  (rec : forall {X Y : Type}, (X -> Y) -> F X -> F Y)
+  (f : A -> B) (t : BushF F A) : BushF F B :=
+match t with
+| E => E
+| N v t' => N (f v) (rec (rec f) t')
+end.
