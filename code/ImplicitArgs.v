e651273a07c304266ac101ad22e19b4054ea3653
wkolowski <wkolowski@protonmail.com>
2023-05-26 23:32:45 +0200

    Kawałek kodu dot. argumentów domyślnych.

---
+++ code/ImplicitArgs.v
@@ -0,0 +1,37 @@
+Require Import Nat.
+
+Variables n : nat.
+
+About sub.
+
+Eval cbn in sub 0 0.
+Eval cbn in sub 0 2.
+Eval cbn in sub 2 1.
+
+Arguments sub !n !m.
+
+Eval cbn in sub n 0.
+Eval cbn in sub 0 n.
+Eval cbn in sub n 1.
+Eval cbn in sub (S n) 1.
+
+Arguments sub !n m.
+
+Eval cbn in sub n 0.
+Eval cbn in sub 0 n.
+Eval cbn in sub n 1.
+Eval cbn in sub (S n) 1.
+
+Arguments sub n !m.
+
+Eval cbn in sub n 0.
+Eval cbn in sub 0 n.
+Eval cbn in sub n 1.
+Eval cbn in sub (S n) 1.
+
+Arguments sub n m : simpl nomatch.
+
+Eval cbn in sub n 0.
+Eval cbn in sub 0 n.
+Eval cbn in sub n 1.
+Eval cbn in sub (S n) 1.
